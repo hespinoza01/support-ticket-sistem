932a8a8032f637b0cf77f2d0f144948388d3c3d0
Harold Espinoza <haroldesptru@gmail.com>
2020-07-31 16:40:24 -0600

    Añadidos ajustes en los colores del navbar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar-fixed">
-    <nav class="nav-wrapper navbar-app">
+    <nav class="nav-wrapper navbar-app green">
         <a href="/" class="brand-logo">Support Ticket System</a>
         <ul class="right hide-on-med-and-down">
             <li class="active"><a href="/">Inicio</a></li>
@@ -7,8 +7,8 @@
             <li><a href="/contacto">Contacto</a></li>
             <li><a href="#!" class="dropdown-trigger" data-target="dropdown-usuario">Usuario <i class="material-icons right">arrow_drop_down</i></a></li>
             <ul id="dropdown-usuario" class="dropdown-content">
-                <li><a href="/usuarios/acceso">Acceder</a></li>
-                <li><a href="/usuarios/registro">Registro</a></li>
+                <li><a href="/usuarios/acceso" class="green-text">Acceder</a></li>
+                <li><a href="/usuarios/registro" class="green-text">Registro</a></li>
             </ul>
         </ul>
     </nav>
